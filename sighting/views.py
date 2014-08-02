78fe74b7952f80980c619b299ec59c53c50ae922
Hunter MacDermut <whosebluesanyway@gmail.com>
2014-08-02 17:33:16 -0400

    Bird list accurately shows birds logged-in user has seen, and ignores sightings altogether when user is not logged in

--- sighting/views.py
+++ sighting/views.py
@@ -10,25 +10,26 @@ def bird_list(request):
 	doves_woodpeckers_etc = Bird.objects.filter(family="doves_woodpeckers_etc")
 	birds_of_prey = Bird.objects.filter(family="birds_of_prey")
 	perching_birds = Bird.objects.filter(family="perching_birds")
+	current_birder = get_user(request)
+	sightings = None
+	if not current_birder.is_anonymous():
+		sightings = get_object_or_404(Sighting, birder=current_birder)
 	ctx = {
 		'waterbirds_and_nearshore_birds': waterbirds_and_nearshore_birds,
 		'doves_woodpeckers_etc': doves_woodpeckers_etc,
 		'birds_of_prey': birds_of_prey,
 		'perching_birds': perching_birds,
+		'sightings': sightings
 	}
 	return render(request, 'sighting/bird_list.html', ctx)
 
 def bird_detail(request, bird_name):
-	# grab bird captured by url, display its data
 	bird = get_object_or_404(Bird, slug=bird_name)
-	# grab current logged-in user
 	current_birder = get_user(request)
 	sighting = None
 	ctx = {
 		'bird': bird
 	}
-	# if user has sighting for bird, load form with that data
-	# otherwise, load blank form, allow for creation of sighting
 	if not current_birder.is_anonymous():
 		sighting, created = Sighting.objects.get_or_create(birder=current_birder, bird=bird, defaults={'sighted': False})
 		ctx['sighting'] = sighting
