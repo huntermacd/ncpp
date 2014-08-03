f17f6feebe4570868e8da701ddbd18fd2ff097c3
Hunter MacDermut <whosebluesanyway@gmail.com>
2014-08-02 23:16:01 -0400

    App in working condition, visuals in place, ready to deploy

--- sighting/forms.py
+++ sighting/forms.py
@@ -4,3 +4,7 @@ from .models import Sighting
 class SightingForm(forms.ModelForm):
 	class Meta:
 		model = Sighting
+		widgets = {
+			'birder': forms.HiddenInput(),
+			'bird': forms.HiddenInput(),
+		}
